2387187dbc3d7134b1d9169cdfef0bc46b007d88
Varun Bhanot <varunbhanot.vb@gmail.com>
2026-03-21 23:34:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6268493585-ed21f",
+  "appId": "1:526320518587:web:4a336d97024ee072d0309e",
+  "apiKey": "AIzaSyDx2uIPPFMKM5-aenLfqVSwU5Devo2PRLo",
+  "authDomain": "studio-6268493585-ed21f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "526320518587"
+};
